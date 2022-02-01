da66a5b3bca73001fdba0616c36d01ddbbc2b79a
Trevor - FanshaweC <tvanrys@fanshaweonline.ca>
2022-02-01 13:47:55 -0500

    adding latest class build

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <section id="app">
+    <header>
+      <h1 class="hidden">Welcome to the Chat App</h1>
+      <h2>Chat App with VueCLI, Socket.io and Express</h2>
+    </header>
+
+    <h4>Below this is dynamic content - the router-view</h4>
+    <!-- the router view is the container that accepts components to render per route -->
+    <router-view></router-view>
+    <h4>Above this is dynamic content - the router-view</h4>
+  </section>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,16 +21,15 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+header {
+  text-align: center;
+  background-color: blue;
+  padding: 1em 0.8em;
+  color: white;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+header {
+  .hidden { display: none; }
+  h2 { font-size: 2.5em; margin-bottom: 0.8em; }
 }
 </style>
